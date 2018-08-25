e85c07570d534f432a24486962556449bc895716
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-08-25 12:03:36 -0400

    starter

---
+++ src/actions/shared.js
@@ -0,0 +1,18 @@
+import {receiveUsers} from './users'
+import {receiveQuestions} from './questions'
+import {setAuthedUser} from './authedUser'
+import {_getUsers, _getQuestions} from '../utils/_DATA'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData(){
+    return (dispatch) => {
+        return Promise.all([_getQuestions(), _getUsers()]).then(
+            ([questions, users]) => {
+                dispatch(receiveQuestions(questions))
+                dispatch(receiveUsers(users))
+                dispatch(setAuthedUser(AUTHED_ID))
+            }
+        )
+    }
+}
